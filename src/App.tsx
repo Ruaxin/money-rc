7f73cb42aa2d2c763fb6c5bdbc3423938e19a2d4
rua <1171852209@qq.com>
2020-07-28 15:25:09 +0800

    底部导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Switch,
     Route,
     Link,
     Redirect,
 } from 'react-router-dom'
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`
+const Nav = styled.nav`
+  > ul{
+    display: flex;
+    > li{
+        width: 33.33%;
+        text-align: center;
+        padding: 16px;
+    }
+  }
+`
 
 function App() {
     return (
         <Router>
-            <div>
-                <nav>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from='/' to='/money'/>
+                        <Route path='*'>
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+                <Nav>
                     <ul>
                         <li>
                             <Link to="/">money</Link>
@@ -24,23 +61,8 @@ function App() {
                             <Link to="/statistics">Statistics</Link>
                         </li>
                     </ul>
-                </nav>
-                <Switch>
-                    <Route path="/money">
-                        <Money/>
-                    </Route>
-                    <Route path="/tags">
-                        <Tags/>
-                    </Route>
-                    <Route path="/statistics">
-                        <Statistics/>
-                    </Route>
-                    <Redirect exact from='/' to='/money'/>
-                    <Route path='*'>
-                        <NoMatch/>
-                    </Route>
-                </Switch>
-            </div>
+                </Nav>
+            </Wrapper>
         </Router>
     )
 }
