7f64ce200ec390a0149c0bbc39ca65d97e2551b5
rua <1171852209@qq.com>
2020-07-28 09:31:02 +0800

    初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import styled from "styled-components";
 
-const Button = styled.button`
-    color:red;
-`
 
 function App() {
     return (
         <div>
-            <Button>你好</Button>
         </div>
     );
 }
