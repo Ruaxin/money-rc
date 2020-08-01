6889329b62048d8f6916bebba55724c12d8920ae
Ruaxin <qy.xin@foxmail.com>
2020-08-01 21:28:16 +0800

    icon完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
-import styled from "styled-components";
-import {Link} from "react-router-dom";
-import React from "react";
-import x from 'icons/money.svg'
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
 
-console.log(x);
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
+
 const NavWrapper = styled.nav`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -12,26 +14,42 @@ const NavWrapper = styled.nav`
     > li{
         width: 33.33%;
         text-align: center;
-        padding: 16px;
+        padding: 4px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .icon{
+          width: 24px;
+          height: 24px;
+        }
     }
   }
-`
+`;
 
 const Nav = () => {
-    return (
-        <NavWrapper>
-            <ul>
-                <li>
-                    <Link to="/">记账页</Link>
-                </li>
-                <li>
-                    <Link to="/tags">标签页</Link>
-                </li>
-                <li>
-                    <Link to="/statistics">统计页</Link>
-                </li>
-            </ul>
-        </NavWrapper>
-    )
-}
-export default Nav
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <svg className='icon'>
+            <use xlinkHref="#money"/>
+          </svg>
+          <Link to="/">记账页</Link>
+        </li>
+        <li>
+          <svg className='icon'>
+            <use xlinkHref="#tag"/>
+          </svg>
+          <Link to="/tags">标签页</Link>
+        </li>
+        <li>
+          <svg className='icon'>
+            <use xlinkHref="#chart"/>
+          </svg>
+          <Link to="/statistics">统计页</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+export default Nav;
