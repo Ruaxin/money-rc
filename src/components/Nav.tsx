769faac4551d2ae64592330a06e34e95da4a5c68
rua <1171852209@qq.com>
2020-08-03 14:19:17 +0800

    添加activeClassName，换成NavLink

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
@@ -20,6 +20,10 @@ const NavWrapper = styled.nav`
           width: 24px;
           height: 24px;
         }
+        &.selected{
+          color: rgb(255,200,36);
+          box-shadow: 0 0, 0 -1px rgba(0,0,0,0.25);
+        }
       }
     }
   }
@@ -30,22 +34,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Link to="/">
+          <NavLink to="/money" activeClassName="selected">
             <Icon name='money'/>
             记账
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/tags">
+          <NavLink to="/tags" activeClassName="selected">
             <Icon name='tag'/>
             标签
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/statistics">
+          <NavLink to="/statistics" activeClassName="selected">
             <Icon name='chart'/>
             统计
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </NavWrapper>
