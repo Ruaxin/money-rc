10401ed4e520403f4a5995e813e8d68ec05044af
rua <1171852209@qq.com>
2020-08-03 16:14:41 +0800

    完成money的NoteSection布局

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -26,7 +26,24 @@ const TagsSection = styled.section`
   }
 `
 const NoteSection = styled.section`
-
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;  
+  > label{
+  display: flex;
+  align-items: center;
+    > span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    > input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
 `
 const CategorySection = styled.section`
 
@@ -49,7 +66,7 @@ function Money() {
       <NoteSection>
         <label>
           <span>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder='在这里添加备注'/>
         </label>
       </NoteSection>
       <CategorySection>
