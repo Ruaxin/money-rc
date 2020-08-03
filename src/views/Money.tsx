3a8d5c407835f41c4707dd5335155a286f3d9d56
rua <1171852209@qq.com>
2020-08-03 17:25:47 +0800

    完成money的NumberPadSection布局

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -69,7 +69,63 @@ const CategorySection = styled.section`
   }
 `;
 const NumberPadSection = styled.section`
-
+  display: flex;
+  flex-direction: column;
+  > .output{
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                inset 0 5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad{
+    > button{
+      font-size: 18px;
+      float: left;
+      width: 20%;
+      height: 64px;
+      border: none;
+      &.ok{
+        float: right;
+        height: 128px;
+      }
+      &:nth-child(1){
+        background: rgba(238, 171, 64);
+      }
+      &:nth-child(2),
+      &:nth-child(6){
+        background: rgba(232, 154, 56);
+      }
+      &:nth-child(3),
+      &:nth-child(7),
+      &:nth-child(11){
+        background: rgba(232, 144, 54);
+      }
+      &:nth-child(4),
+      &:nth-child(8),
+      &:nth-child(12),
+      &:nth-child(16){
+        background: rgba(228, 125, 48);
+      }
+      &:nth-child(5),
+      &:nth-child(9),
+      &:nth-child(13),
+      &:nth-child(17){
+        background: rgba(223, 108, 43);
+      }
+      &:nth-child(10),
+      &:nth-child(14),
+      &:nth-child(18){
+        background: rgba(226, 103, 44);
+      }
+      &:nth-child(15),
+      &:nth-child(19){
+        background: rgba(219, 98, 41);
+      }
+    }
+  }
 `;
 
 function Money() {
@@ -97,25 +153,28 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
-        <button>删除</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
-        <button>清空</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>×</button>
-        <button>ok</button>
-        <button>0</button>
-        <button>.</button>
-        <button>%</button>
-        <button>÷</button>
+        <div className='output'>100</div>
+        <div className="pad clearfix">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>×</button>
+          <button className='ok'>ok</button>
+          <button>0</button>
+          <button>.</button>
+          <button>%</button>
+          <button>÷</button>
+        </div>
       </NumberPadSection>
     </Layout>
   );
