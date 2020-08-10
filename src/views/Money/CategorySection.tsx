a5afea03e86988551bfe3b0eeed60fbf6ed04666
Ruaxin <qy.xin@foxmail.com>
2020-08-10 09:24:06 +0800

    完成category功能

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const CategorySection = styled.section`
+const Wrapper = styled.section`
   font-size: 24px;
   > ul{
     display: flex;
@@ -14,7 +15,7 @@ const CategorySection = styled.section`
         content: '';
         height: 3px;
         width: 100%;
-        background: rgb(255, 80, 0);
+        background: black;
         display: block; 
         position: absolute;
         bottom: 0;
@@ -23,4 +24,21 @@ const CategorySection = styled.section`
     }
   }
 `;
+const CategorySection: React.FC = () => {
+  const categoryMap = {'-': '支出', '+': '收入'};
+  const [categoryList] = useState<('-' | '+')[]>(['-', '+']);
+  const [category, setCategory] = useState('-');
+  return (
+    <Wrapper>
+      <ul>
+        {categoryList.map(c =>
+          <li key={c} className={category === c ? 'selected' : ''}
+              onClick={() => setCategory(c)}>
+            {categoryMap[c]}
+          </li>
+        )}
+      </ul>
+    </Wrapper>
+  );
+};
 export {CategorySection};
