fc8af3a9e4c20f019ec6d4e82f2aa88a08caaee2
Ruaxin <qy.xin@foxmail.com>
2020-08-17 21:20:31 +0800

    对updateTag和deleteTag进行重构

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -24,20 +24,11 @@ const useTags = () => {//封装一个自定义Hook
       return result;
     }
   ;
-  const updateTag = (id: number, obj: { name: string }) => {
-    //获取要改的 tag 的下标
-    const index = findTagIndex(id);
-    //深拷贝 tags 得到tagsClone
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    //把 tagsClone 的第 index 删掉，换成 {id:id,name:obj.name}
-    tagsClone.splice(index, 1, {id: id, name: obj.name});
-    setTags(tagsClone);
+  const updateTag = (id: number, {name}: { name: string }) => {
+    setTags(tags.map(tag => tag.id === id ? {id, name} : tag));
   };
   const deleteTag = (id: number) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   };
   return {tags, setTags, findTag, findTagIndex, updateTag, deleteTag};
 };
