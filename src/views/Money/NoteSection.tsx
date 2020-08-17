8e2398e097c7e384a600edbf88b85c1e4085fbe5
Ruaxin <qy.xin@foxmail.com>
2020-08-17 14:30:47 +0800

    封装center和space

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -4,23 +4,9 @@ import {Input} from 'components/Input';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
-  padding: 0 16px;
+  padding: 14px 16px;
   font-size: 14px;  
-  > label{
-    display: flex;
-    align-items: center;
-    > span{
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    > input{
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
+
 `;
 
 type Props = {
@@ -38,6 +24,7 @@ const NoteSection: React.FC<Props> = (props) => {
       <label>
         <Input label='备注'
                type='text'
+               placeholder='请填写备注'
                value={note}
                onChange={onChange}
         />
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -5,6 +5,9 @@ import Layout from 'components/Layout';
 import Icon from 'components/Icon';
 import {Button} from 'components/Button';
 import styled from 'styled-components';
+import {Input} from 'components/Input';
+import {Center} from 'components/Center';
+import {Space} from 'components/Space';
 
 const Topbar = styled.header`
   display: flex;
@@ -12,6 +15,11 @@ const Topbar = styled.header`
   line-height: 20px;
   padding: 14px;
   align-items: center;
+`;
+const InputWrapper = styled.div`
+  background: white;
+  padding: 0 16px;
+  margin-top: 16px;
 `;
 
 type Params = {
@@ -29,17 +37,15 @@ const Tag: React.FC = () => {
         <span>编辑标签</span>
         <Icon/>
       </Topbar>
-      <div>
-        <label>
-          <span>标签名</span>
-          <input type="text"
-                 placeholder='标签名'
-          />
-        </label>
-      </div>
-      <div>
+      <InputWrapper>
+        <Input label='标签名' type='text' placeholder='请输入标签名'/>
+      </InputWrapper>
+      <Center>
+        <Space/>
+        <Space/>
+        <Space/>
         <Button>删除标签</Button>
-      </div>
+      </Center>
     </Layout>
   );
 };
