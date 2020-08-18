f3256f60348b960df6ce9869e36eaff1642c649b
Ruaxin <qy.xin@foxmail.com>
2020-08-18 13:16:26 +0800

    完善tags功能，增加数据持久化

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,36 +1,50 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {createId} from 'lib/createId';
+import {useUpdate} from './hooks/useUpdate';
 
-const defaultTags =
-  [
-    {id: createId(), name: '衣'},
-    {id: createId(), name: '食'},
-    {id: createId(), name: '住'},
-    {id: createId(), name: '行'},
-  ];
 const useTags = () => {//封装一个自定义Hook
-  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+  const [tags, setTags] = useState<{ id: number; name: string }[]>([]);
+  useEffect(() => {
+    let localTags = JSON.parse(window.localStorage.getItem('tags') || '[]');
+    if (localTags.length === 0) {
+      localTags = [
+        {id: createId(), name: '衣'},
+        {id: createId(), name: '食'},
+        {id: createId(), name: '住'},
+        {id: createId(), name: '行'},
+      ];
+    }
+    setTags(localTags);
+  }, []);//组件挂载时执行
+  useUpdate(() => {
+    window.localStorage.setItem('tags', JSON.stringify(tags));
+  }, [tags]);
   const findTag = (id: number) => {
     return tags.filter(tag => tag.id === id)[0];
   };
   const findTagIndex = (id: number) => {
-      let result = -1;
-      for (let i = 0; i < tags.length; i++) {
-        if (tags[i].id === id) {
-          result = i;
-          break;
-        }
+    let result = -1;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
       }
-      return result;
     }
-  ;
+    return result;
+  };
   const updateTag = (id: number, {name}: { name: string }) => {
     setTags(tags.map(tag => tag.id === id ? {id, name} : tag));
   };
   const deleteTag = (id: number) => {
     setTags(tags.filter(tag => tag.id !== id));
   };
-  return {tags, setTags, findTag, findTagIndex, updateTag, deleteTag};
+  const addTag = () => {
+    const tagName = window.prompt('请输入标签');
+    if (tagName !== null && tagName !== '') {
+      setTags([...tags, {id: createId(), name: tagName}]);
+    }
+  };
+  return {tags, setTags, findTag, findTagIndex, updateTag, deleteTag, addTag};
 };
 
 export {useTags};
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import React from 'react';
 import {useTags} from 'useTags';
-import {createId} from 'lib/createId';
 
 const Wrapper = styled.section`
   background: #ffffff;
@@ -41,14 +40,9 @@ type Props = {
 }
 
 const TagsSection: React.FC<Props> = (props) => {
-  const {tags, setTags} = useTags();
+  const {tags, addTag} = useTags();
   const selectedTagIds = props.value;
-  const onAddTag = () => {
-    const tagName = window.prompt('请输入标签');
-    if (tagName !== null) {
-      setTags([...tags, {id: createId(), name: tagName}]);
-    }
-  };
+
   const onToggleTag = (tagId: number) => {
     const index = selectedTagIds.indexOf(tagId);
     if (index >= 0) {
@@ -70,7 +64,7 @@ const TagsSection: React.FC<Props> = (props) => {
           </li>
         )}
       </ol>
-      <button onClick={onAddTag}>新增标签</button>
+      <button onClick={addTag}>新增标签</button>
     </Wrapper>
   );
 };
