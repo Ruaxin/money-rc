62125d8678ea360cfd7d5cf2c70f0253b8f8c822
Ruaxin <qy.xin@foxmail.com>
2020-08-19 11:22:55 +0800

    records新增记录时间功能

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -5,8 +5,10 @@ type RecordItem = {
   tagIds: number[];
   note: string;
   category: '+' | '-';
-  amount: number
+  amount: number;
+  createAt: string //ISO 8601
 }
+type newRecordItem = Omit<RecordItem, 'createAt'>
 
 const useRecords = () => {
   const [records, setRecords] = useState<RecordItem[]>([]);
@@ -16,7 +18,8 @@ const useRecords = () => {
   useUpdate(() => {
     window.localStorage.setItem('records', JSON.stringify(records));
   }, [records]);
-  const addRecord = (record: RecordItem) => {
+  const addRecord = (newRecord: newRecordItem) => {
+    const record = {...newRecord, createAt: (new Date()).toISOString()};
     setRecords([...records, record]);
   };
   return {records, addRecord};
