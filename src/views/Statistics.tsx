2a02ba2e58ac6eab3fa67614a534f5b18c9d3c35
Ruaxin <qy.xin@foxmail.com>
2020-08-21 08:49:39 +0800

    完善记录样式

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -9,6 +9,19 @@ import day from 'dayjs';
 const CategoryWrapper = styled.div`
     background: #f0cf5e;
 `;
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background: white;
+  font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+  > .note{
+    margin-right: auto;
+    margin-left: 16px;
+    color: #999;
+  }
+`;
 
 function Statistics() {
   const [category, setCategory] = useState<'+' | '-'>('-');
@@ -20,19 +33,22 @@ function Statistics() {
         <CategorySection value={category}
                          onChange={value => setCategory(value)}
         />
-        <div>
-          {records.map(r => {
-            return (
-              <div>
-                {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
-                <hr/>
-                {r.amount}
-                <hr/>
-                {day(r.createAt).format('YYYY年MM月DD日')}
-              </div>);
-          })}
-        </div>
       </CategoryWrapper>
+      <div>
+        {records.map(r => {
+          return (
+            <Item>
+              <div className="tags">
+                {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+              </div>
+              {r.note && <div className="note">{r.note}</div>}
+              <div className="amount">
+                ￥{r.amount}
+              </div>
+              {/*{day(r.createAt).format('YYYY年MM月DD日')}*/}
+            </Item>);
+        })}
+      </div>
     </Layout>
   );
 }
