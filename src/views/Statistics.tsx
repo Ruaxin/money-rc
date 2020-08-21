ff2eab15f7ec1e933ce2311460518d13aa745974
Ruaxin <qy.xin@foxmail.com>
2020-08-21 13:11:12 +0800

    处理展示记录数据

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -2,7 +2,7 @@ import Layout from 'components/Layout';
 import React, {useState} from 'react';
 import {CategorySection} from './Money/CategorySection';
 import styled from 'styled-components';
-import {useRecords} from '../hooks/useRecords';
+import {RecordItem, useRecords} from '../hooks/useRecords';
 import {useTags} from '../hooks/useTags';
 import day from 'dayjs';
 
@@ -27,6 +27,22 @@ function Statistics() {
   const [category, setCategory] = useState<'+' | '-'>('-');
   const {records} = useRecords();
   const {getName} = useTags();
+  const selectedRecords = records.filter(r => r.category === category);
+  const hash: { [K: string]: RecordItem[] } = {};
+  selectedRecords.map(r => {
+    const key = day(r.createAt).format('YYYY-MM-DD');
+    if (!(key in hash)) {
+      hash[key] = [];
+    }
+    hash[key].push(r);
+  });
+  const array = Object.entries(hash).sort((a, b) => {
+    if (a[0] === b[0]) return 0;
+    if (a[0] > b[0]) return 1;
+    if (a[0] < b[0]) return -1;
+    return 0;
+  });
+  console.log(array);
   return (
     <Layout>
       <CategoryWrapper>
@@ -35,17 +51,17 @@ function Statistics() {
         />
       </CategoryWrapper>
       <div>
-        {records.map(r => {
+        {selectedRecords.map(r => {
           return (
-            <Item>
+            <Item key={r.createAt}>
               <div className="tags">
-                {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
+                {r.tagIds.map(tagId => <span key={tagId}>{getName(tagId)}</span>)}
               </div>
               {r.note && <div className="note">{r.note}</div>}
               <div className="amount">
                 ￥{r.amount}
               </div>
-              {/*{day(r.createAt).format('YYYY年MM月DD日')}*/}
+              {day(r.createAt).format('YYYY年MM月DD日')}
             </Item>);
         })}
       </div>
